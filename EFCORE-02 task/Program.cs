e0d36e50a69803c49747b85ae001f0a79484e131
Issa Al Sibani <issaalsibani12@gmail.com>
2026-07-27 21:49:10 +0400

    Add project files.

---
+++ EFCORE-02 task/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCORE_02_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
